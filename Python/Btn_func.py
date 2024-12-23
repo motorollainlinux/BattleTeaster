0a6e8de7e77020da6612a5a4581b625a497aadba
Kirill <kirya.samoylov.06@list.ru>
2024-12-24 03:48:59 +0700

    Add switching bettwen forms

--- Python/Btn_func.py
+++ Python/Btn_func.py
@@ -1,1 +1,50 @@
 from PyQt5 import QtCore, QtWidgets, uic
+#          Menu         BS-B        BS-P        Battle      Settings    Leaders
+Sizes = [[295, 369], [310, 330], [338, 330], [464, 579], [385, 295], [549, 744]]
+class BattleTeaster(QtWidgets.QMainWindow):
+    def __init__(self, parent = None):
+        super().__init__(parent)
+        uic.loadUi("./Python/Menu.ui", self)
+        self.pushButton1.clicked.connect(self.ChangeToBotBattleSettings)
+        self.pushButton2.clicked.connect(self.ChangeToPlayerBattleSettings)
+        self.pushButton3.clicked.connect(self.ChangeToSettings)
+        self.pushButton4.clicked.connect(self.ChangeToLeaders)
+        self.setMinimumSize(0, 0)
+        self.resize(Sizes[0][0], Sizes[0][1])
+    def ChangeToBotBattleSettings(self) -> None:
+        uic.loadUi("./Python/BS-B.ui", self)
+        self.pushButton4.clicked.connect(self.ChangeToMenu)
+        self.setMinimumSize(0, 0)
+        self.resize(Sizes[1][0], Sizes[1][1])
+    def ChangeToPlayerBattleSettings(self) -> None:
+        uic.loadUi("./Python/BS-P.ui", self)
+        self.pushButton4.clicked.connect(self.ChangeToMenu)
+        self.setMinimumSize(0, 0)
+        self.resize(Sizes[2][0], Sizes[2][1])
+    def ChangeToSettings(self) -> None:
+        uic.loadUi("./Python/Settings.ui", self)
+        self.pushButton.clicked.connect(self.ChangeToMenu)
+        self.setMinimumSize(0, 0)
+        self.resize(Sizes[4][0], Sizes[4][1])
+    def ChangeToBattle(self) -> None:
+        uic.loadUi("./Python/Battle.ui", self)
+        self.pushButton2.clicked.connect(self.ChangeToMenu)
+        self.setMinimumSize(0, 0)
+        self.resize(Sizes[3][0], Sizes[3][1])
+    def ChangeToLeaders(self) -> None:
+        uic.loadUi("./Python/Leaders.ui", self)
+        self.pushButton1.clicked.connect(self.ChangeToMenu)
+        self.setMinimumSize(0, 0)
+        self.resize(Sizes[5][0], Sizes[5][1])
+    def ChangeToMenu(self) -> None:
+        uic.loadUi("./Python/Menu.ui", self)
+        self.pushButton1.clicked.connect(self.ChangeToBotBattleSettings)
+        self.pushButton2.clicked.connect(self.ChangeToPlayerBattleSettings)
+        self.pushButton3.clicked.connect(self.ChangeToSettings)
+        self.pushButton4.clicked.connect(self.ChangeToLeaders)
+        self.setMinimumSize(0, 0)
+        self.resize(Sizes[0][0], Sizes[0][1])
+        
+        
+        
+        
--- Python/main.py
+++ Python/main.py
@@ -1,13 +1,8 @@
 from PyQt5 import QtCore, QtWidgets, uic
 import sys, Btn_func
-
-class BattleTeaster(QtWidgets.QWidget):
-    def __init__(self, parent = None):
-        super().__init__(parent)
-        uic.loadUi("Battle-Teaster.ui", self)
-    
+ 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    Window = BattleTeaster()
+    Window = Btn_func.BattleTeaster()
     Window.show()
     sys.exit(app.exec_())
